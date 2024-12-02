9dc1b3cf527c40b063eecb2b9bb60e767d858c16
Ethan THIERRY <ethan.thierry@epitech.eu>
2024-12-02 16:55:15 +0100

    feat: implemented string parsing & base StringFormatter class

---
+++ src/classes/format/StringFormatter.ts
@@ -0,0 +1,55 @@
+export default class StringFormatter {
+
+    public resolve(object: any): string {
+        return '[DEFAULT FORMATTER]'
+    }
+
+}
+
+export class FormattedString {
+
+    private parts: (string | StringFormatter)[] = []
+
+    private constructor(parts: (string | StringFormatter)[]) {
+        this.parts = parts
+    }
+    public static create(string: string): FormattedString {
+        const parts: (string | StringFormatter)[] = [];
+        let index = 0
+        while (index < string.length) {
+            const start = string.indexOf('%', index)
+            if (start === -1) {
+                parts.push(string.substring(index))
+                break
+            }
+            parts.push(string.substring(index, start))
+            const end = string.indexOf('%', start + 1)
+            if (end === -1) {
+                parts.push(string.substring(start))
+                throw new Error('No closing marker found')
+            }
+            const expression = string.substring(start + 1, end)
+            parts.push(this.parseExpression(expression))
+            index = end + 1
+        }
+
+        return new FormattedString(parts)
+    }
+
+    private static parseExpression(expression: string): StringFormatter {
+        return new StringFormatter()
+    }
+
+    public resolve(object: any): string {
+        let result = ''
+        for (const part of this.parts) {
+            if (part instanceof StringFormatter) {
+                result += part.resolve(object)
+            } else {
+                result += part
+            }
+        }
+        return result
+    }
+
+}
